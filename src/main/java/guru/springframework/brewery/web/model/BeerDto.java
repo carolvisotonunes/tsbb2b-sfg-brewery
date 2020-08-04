49a86852feab47fafd440118f502864292addf2f
Caroline Nunes <carolinenunes@Carolines-MacBook-Pro.local>
2020-08-04 17:32:47 +1000

    Test rest tamplate

--- src/main/java/guru/springframework/brewery/web/model/BeerDto.java
+++ src/main/java/guru/springframework/brewery/web/model/BeerDto.java
@@ -17,14 +17,21 @@
 
 package guru.springframework.brewery.web.model;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.JsonNode;
 import lombok.Builder;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 
 import java.math.BigDecimal;
 import java.time.OffsetDateTime;
+import java.util.List;
 import java.util.UUID;
 
 @Data
@@ -51,4 +58,29 @@ public class BeerDto extends BaseItem {
     @JsonFormat(shape= JsonFormat.Shape.STRING)
     private BigDecimal price;
 
+    public static class BeerPagedList extends PageImpl<BeerDto> {
+        @JsonCreator(mode = JsonCreator.Mode.PROPERTIES)
+        public BeerPagedList(@JsonProperty("content") List<BeerDto> content,
+                             @JsonProperty("number") int number,
+                             @JsonProperty("size") int size,
+                             @JsonProperty("totalElements") Long totalElements,
+                             @JsonProperty("pageable") JsonNode pageable,
+                             @JsonProperty("last") boolean last,
+                             @JsonProperty("totalPages") int totalPages,
+                             @JsonProperty("sort") JsonNode sort,
+                             @JsonProperty("first") boolean first,
+                             @JsonProperty("numberOfElements") int numberOfElements) {
+
+            super(content, PageRequest.of(number, size), totalElements);
+        }
+
+        public BeerPagedList(List<BeerDto> content, Pageable pageable, long total) {
+            super(content, pageable, total);
+        }
+
+        public BeerPagedList(List<BeerDto> content) {
+            super(content);
+        }
+
+    }
 }
--- target/generated-sources/annotations/guru/springframework/brewery/web/mappers/BeerMapperImpl.java
+++ target/generated-sources/annotations/guru/springframework/brewery/web/mappers/BeerMapperImpl.java
@@ -10,7 +10,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2020-08-02T23:40:16+1000",
+    date = "2020-08-04T16:40:49+1000",
     comments = "version: 1.3.1.Final, compiler: javac, environment: Java 12.0.1 (Oracle Corporation)"
 )
 @Component
